d4e842d01736eb910c83d6de321c4cb57d83fb56
Abduali228 <54325881+Abduali228@users.noreply.github.com>
2021-01-20 13:28:49 +0600

    Add files via upload

---
+++ Abu/Abu/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  Abu
+//
+//  Created by Abu nakhu
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var loginLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    @IBOutlet weak var signInButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func signIn(_ sender: UIButton) {
+    }
+}
+
